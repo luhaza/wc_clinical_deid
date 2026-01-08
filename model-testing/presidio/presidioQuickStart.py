308b57bfd774114f008ae716cef8c42eca11a452
lukezann <luke.zanuck@gmail.com>
2026-01-08 15:57:01 -0500

    sample data

--- model-testing/presidio/presidioQuickStart.py
+++ model-testing/presidio/presidioQuickStart.py
@@ -1,18 +1,23 @@
 from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
-text=""
+
+
+
+file_loc = "../sample data/lab_result.txt"
+text = open(file_loc).read()
+print(text, "----------")
 
 # Set up the engine, loads the NLP module (spaCy model by default) 
 # and other PII recognizers
 analyzer = AnalyzerEngine()
 
 # Call analyzer to get results
-results = analyzer.analyze(text=text,
-                           entities=["PERSON", "PHONE_NUMBER", "DATE_TIME"],
-                           language='en')
-
-print(text)
-print(results)
+results = analyzer.analyze(text=text, 
+                           entities=['PERSON', 'PHONE_NUMBER', 'MEDICAL_LICENSE','LOCATION', 'IP_ADDRESS',
+                                     'EMAIL_ADDRESS', 'US_SSN', 'US_DRIVER_LICENSE',
+                                     'CREDIT_CARD', 'US_BANK_NUMBER', 'US_ITIN', 'US_PASSPORT'], 
+                           language='en',
+                           )
 
 # Analyzer results are passed to the AnonymizerEngine for anonymization
 
