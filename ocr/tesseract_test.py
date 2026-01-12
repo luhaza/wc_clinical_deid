5b6f27e078d1aaf15c736d1e7295bc16a3a5782a
Xin Qiu <xq2706@gmail.com>
2026-01-12 09:30:01 -0500

    added pdf compatability w/ tesseract

--- ocr/tesseract_test.py
+++ ocr/tesseract_test.py
@@ -3,6 +3,7 @@ import sys
 import os
 from PIL import Image
 import pytesseract
+from pdf2image import convert_from_path
 
 
 output_directory = "ocr_output"
@@ -10,35 +11,57 @@ output_directory = "ocr_output"
 def main():
     # checking for command line arguments
     if len(sys.argv) < 2:
-        print("check arguments: python run_ocr.py <image_file>")
+        print("check arguments: python run_ocr.py <input_file>")
         sys.exit(1)
 
-    image_path = sys.argv[1]
+    input_path = sys.argv[1]
+    extension = os.path.splitext(input_path)[1].lower()
+    
+    images = [] #stores image/pdf page images to convert to PIL
 
     # open the image --> converting to Pillow img object to pass to tess
     # (raw image data)
-    try:
-        image = Image.open(image_path)
-    except Exception as e:
-        print(f"Error opening image '{image_path}': {e}")
-        sys.exit(1)
 
-    # performing OCR
-    # pytesseract will find Tesseract automatically via PATH
-    text = pytesseract.image_to_string(image)
+    #check if input is a pdf first (needs conversion)
+    if extension == ".pdf":
+        try:
+            images = convert_from_path(input_path, dpi=300) # converts each page to PIL
+        except Exception as e:
+            print(f"Error converting PDF '{input_path}': {e}")
+            sys.exit(1)
 
-    # checking output directory exists, creating output file
-    os.makedirs(output_directory, exist_ok=True)
+    else:
+        try:
+            image = Image.open(input_path)
+            images = [image]
+        except Exception as e:
+            print(f"Error opening image '{input_path}': {e}")
+            sys.exit(1)
 
-    # output text file has same name as input image file
-    base_name = os.path.splitext(os.path.basename(image_path))[0]
-    output_file_path = os.path.join(output_directory, f"{base_name}.txt")
+    # performing OCR on each image
+    os.makedirs(output_directory, exist_ok=True) # checking output directory exists, creating output file
+    base_name = os.path.splitext(os.path.basename(input_path))[0]
 
-    # writing resutls to output file
-    with open(output_file_path, "w", encoding="utf-8") as out:
-        out.write(text)
+    page_num = 1 # starting page count w/ 1
 
-    print(f"Result saved to: {output_file_path}")
+    for image in images:
+        text = pytesseract.image_to_string(image)
+
+        if len(images)>1:
+            output_file = f"{base_name}_page{page_num}.txt"
+        else:
+            output_file = f"{base_name}.txt"
+        
+
+        output_file_path = os.path.join(output_directory, output_file)
+
+        # writing resutls to output file
+        with open(output_file_path, "w", encoding="utf-8") as out:
+            out.write(text)
+
+        page_num += 1
+
+    print(f"Results saved to ocr_output")
 
 if __name__ == "__main__":
     main()
